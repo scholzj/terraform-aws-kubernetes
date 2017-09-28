47906fb64325097b8881a51b71fb94a0693947d7
Jakub Scholz <www@scholzj.com>
2017-09-28 17:20:54 +0200

    Rename kubernetes .tf to maint.tf and merge aws.tf into it

--- aws.tf
+++
@@ -1,4 +0,0 @@
-# Retrieve AWS credentials from env variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-provider "aws" {
-  region = "${var.aws_region}"
-}
--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 #####
+# AWS Prodvider
+#####
+
+# Retrieve AWS credentials from env variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+#####
 # Security Group
 #####
 
