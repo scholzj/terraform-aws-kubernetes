431fbde9038b638cf99092d820a84cff4f33d76f
Kyle Smith <kylesmith@pillartechnology.com>
2018-06-08 14:27:42 -0400

    remove whitespace changes

--- outputs.tf
+++ outputs.tf
@@ -3,31 +3,31 @@
 #####
 
 output "ssh_user" {
-  description = "SSH user to download kubeconfig file"
-  value       = "centos"
+    description = "SSH user to download kubeconfig file"
+    value = "centos"
 }
 
 output "public_ip" {
-  description = "Cluster IP address"
-  value       = "${aws_eip.master.public_ip}"
+    description = "Cluster IP address"
+    value = "${aws_eip.master.public_ip}"
 }
 
 output "private_ip" {
-  description = "Cluster private IP address"
-  value       = "${aws_instance.master.private_ip}"
+    description = "Cluster private IP address"
+    value = "${aws_instance.master.private_ip}"
 }
 
 output "dns" {
-  description = "Cluster DNS address"
-  value       = "${aws_route53_record.master.fqdn}"
+    description = "Cluster DNS address"
+    value = "${aws_route53_record.master.fqdn}"
 }
 
 output "kubeconfig_dns" {
-  description = "Path to the the kubeconfig file using DNS address"
-  value       = "/home/centos/kubeconfig"
+    description = "Path to the the kubeconfig file using DNS address"
+    value = "/home/centos/kubeconfig"
 }
 
 output "kubeconfig_ip" {
-  description = "Path to the kubeconfig file using IP address"
-  value       = "/home/centos/kubeconfig_ip"
+    description = "Path to the kubeconfig file using IP address"
+    value = "/home/centos/kubeconfig_ip"
 }
