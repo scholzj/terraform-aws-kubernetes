5f770e1b40aaa7a691d717f8f8f12419f150b806
Jakub Scholz <www@scholzj.com>
2021-01-17 18:41:55 +0100

    Update to Kubernetes 1.20.2

--- scripts/init-aws-kubernetes-master.sh
+++ scripts/init-aws-kubernetes-master.sh
@@ -16,7 +16,7 @@ export ASG_MAX_NODES="${asg_max_nodes}"
 export AWS_REGION=${aws_region}
 export AWS_SUBNETS="${aws_subnets}"
 export ADDONS="${addons}"
-export KUBERNETES_VERSION="1.20.1"
+export KUBERNETES_VERSION="1.20.2"
 
 # Set this only after setting the defaults
 set -o nounset
--- scripts/init-aws-kubernetes-node.sh
+++ scripts/init-aws-kubernetes-node.sh
@@ -9,7 +9,7 @@ set -o pipefail
 export KUBEADM_TOKEN=${kubeadm_token}
 export MASTER_IP=${master_private_ip}
 export DNS_NAME=${dns_name}
-export KUBERNETES_VERSION="1.20.1"
+export KUBERNETES_VERSION="1.20.2"
 
 # Set this only after setting the defaults
 set -o nounset
